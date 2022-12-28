ec8605ff1c9ca318475eb24189cc4046745c8990
Santiago Fiorotto <santifiorotto@hotmail.com>
2022-12-27 21:02:04 -0300

    Mar27-12

--- Desafio2/desafio2.js
+++ Desafio2/desafio2.js
@@ -1,5 +1,9 @@
+const fs = require ("node:fs");
+
 class ProductManager {
-    constructor() {
+  productos;
+    constructor(path) {
+      this.path = path;
       this.products = [];
       this.idCounter = 0;
     }
@@ -25,6 +29,10 @@ class ProductManager {
         code,
         stock
       });
+
+      this.guardarEnArchivo();
+
+
     }
   //Devolver el arreglo con todos los productos creados hasta el momento
     getProducts() {
@@ -41,17 +49,31 @@ class ProductManager {
   //Si no devolver el producto que coincida con el ID
       return product;
     }
+
+    guardarEnArchivo(){
+      try {
+          fs.writeFileSync(this.path, JSON.stringify(this.productos));
+      } catch (err) {
+          throw new Error(err);
+      }
+  }
+
+  cargarElArchivo() {
+      try {
+          this.productos = JSON.parse(fs.readFileSync(this.path,"utf-8"))
+      } catch (err){
+          throw new Error (err);
+      }
+  }
   }
 
 //CREAMOS UNA INSTANCIA
 
-const productManager = new ProductManager();
+const productManager = new ProductManager("./Desafio2/productos.json");
 
+//PRODUCTOS
 productManager.addProduct('Pantufla', 'Pantufl Dama Abierta', 1810, 'https://res.cloudinary.com/djjmhiwzd/image/upload/v1669244037/comprimida_VERANO_COMUNES_excqpp.png', 'D-01', 4);
-
 productManager.addProduct('Zapatillas', 'Zapatillas de hombre', 5000, 'https://res.cloudinary.com/djjmhiwzd/image/upload/v1670891783/vecteezy_sneakers-shoes-clipart-design-illustration_9399188_732_q1sauo.png', 'Zap-4', 65);
-
-
 productManager.addProduct('Remeras', 'Remera de hombre', 3200, 'https://res.cloudinary.com/djjmhiwzd/image/upload/v1670891788/vecteezy_isolated-blue-t-shirt_8847305_174_jlde7t.png', 'Rem-3', 56);
 
 const allProducts = productManager.getProducts();
@@ -59,3 +81,33 @@ console.log(allProducts);
 
 const productById = productManager.getProductById(2);
 console.log(productById);
+
+
+
+
+
+/*
+    
+//PAra actualizar calculo que se usa el método write debido a que escribe o sobre escribe
+//Como dice que recibe el id y el campo, calculo que debe recibir como argumento el id y el campo productos entero.
+    upDateProduct(id, productos){
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.productos));
+        } catch (err) {
+            throw new Error(err);
+        }
+
+    }
+    
+//El método para borrar es unlync, pero el profe explico como borrar un archivo entero, no se como borrar solo una parte de un archivo.
+    deleteProduct(id){
+        fs.unlinkSync("")
+    }
+
+
+}
+
+
+}
+
+*/
